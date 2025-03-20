ee8f32647866aa40cb64e470c66ed34474ba3dcf
Camilo <ch385593@gmail.com>
2025-03-20 16:26:59 -0500

    modificando prestamos

--- frontendbibliotecapublica/src/prestamos/CrearPrestamos.jsx
+++ frontendbibliotecapublica/src/prestamos/CrearPrestamos.jsx
@@ -1,9 +1,34 @@
-
+import { useState } from "react";
 function CrearPrestamos() {
+  const [usuario_id,setUsuario_id]=useState("");
+  const [libro_id,setLibro_id]=useState("");
+  const [fecha_prestamo,setFecha_prestamo]=useState("");
+  const [fecha_devolucion,setFecha_devolucion]=useState("");
+  const [estado,setEstado]=useState("");
+  const handleSubmit=async()=>{ 
+    const respuesta=await fetch("http://localhost:3000/api/Prestamo/",{
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify({usuario_id,libro_id,fecha_prestamo,fecha_devolucion,estado})
+    })
+  }
   return (
     <div>
-      <h1>Crear Prestamos</h1>
-    </div>
+        <h1>Crear Prestamos</h1>
+        <form onSubmit={handleSubmit}>
+            <label >Usuario id</label>
+            <input type="text" onChange={(event)=>setUsuario_id(event.target.value)} placeholder="Escriba el usuario id" />
+            <label >Libro id</label>
+            <input type="text" onChange={(event)=>setLibro_id(event.target.value)} placeholder="Escriba el libro id" />
+            <label >fecha prestamo</label>
+            <input type="text" onChange={(event)=>setFecha_prestamo(event.target.value)} placeholder="Escriba la fecha de prestamo" />
+            <label >fecha devolucion</label>
+            <input type="text" onChange={(event)=>setFecha_devolucion(event.target.value)} placeholder="Escriba la fecha de devolucion" />
+            <label >estado</label>
+            <input type="text" onChange={(event)=>setEstado(event.target.value)} placeholder="Escriba el estado" />
+            <button type="submit">Registrar Reserva</button>
+        </form>
+        </div>
   )
 }
 
